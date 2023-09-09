ac4bc5ff998f3d2ca31867f6d327aacb7d1d01f8
[Grzegorz Kmiotek] <grzegorz.kmiotek+grupa41@gmail.com>
2023-09-09 09:55:18 +0200

    added

--- index.js
+++ index.js
@@ -1,42 +1,77 @@
-const incomeForm = document.getElementById("income-form");
+const incomes = [];
+const expenses = [];
 
-const uiSelectors = {
-	switchInput: "switch",
-	descriptionInput: "description",
-	valueInput: "value",
-	enterButton: "[data-enter-button]",
-	balanceList: "[data-balance-list]",
-	balanceListIncomes: "[data-balance-list-incomes]",
-	balanceListExpenses: "[data-balance-list-expenses]",
-	itemDescription: "[data-item-description]",
-	itemValue: "[data-item-value]",
-	deleteButton: "[data-delete-button]",
-	editButton: "[data-edit-button]",
-	totalBudgetInfo: "[data-total-budget-info]",
+const renderList = (items, listId) => {
+	const list = document.querySelector(listId);
+	list.innerHTML = "";
+	list.classList.remove("removeLi");
+	items.forEach((element) => {
+		const item = document.createElement("li");
+		const itemText = document.createTextNode(element.name + " - ");
+		const itemNum = document.createTextNode(element.amount);
 
-	createItem: "[create-item]",
-	error: "[data-error]",
+		item.appendChild(itemText);
+		item.appendChild(itemNum);
+		list.appendChild(item);
+
+		const btnRemove = document.createElement("button");
+		btnRemove.style.float = "right";
+		btnRemove.innerHTML = "Usuń";
+		item.appendChild(btnRemove);
+
+		
+		const btnEdit = document.createElement("button");
+		btnEdit.style.float = "right";
+		btnEdit.style.marginRight = "10px";
+		btnEdit.innerHTML = "Edytuj";
+		item.appendChild(btnEdit);
+	});
 };
 
-incomeForm.addEventListener("submit", () => {});
-
-function createItem(id, description, price) {
-	return `
-	<li id="${id}">
-			<p>${description}</p>
-			<p>" - "</p>
-			<p>${price}</p>
-		<button class="float" onclick="addIncome()" data-delete-button>Usuń</button>
-		<button class="float" data-enter-button onclick="addIncome()" data-edit-button><p>Edytuj</p></button>
-	</li>
-
-<p class="error hide" data-error>Description can't be empty, value must be positive number</p>
-	`;
+function removeItem() {
+	
 }
 
-function showError() {
-	this.error.clasList.remove("hide");
-}
-function hideError() {
-	this.error.clasList.add("hide");
-}
+
+document
+	.querySelector("#income-form")
+	.addEventListener("submit", function (event) {
+		event.preventDefault();
+		const inputText = document.querySelector("#income-text");
+		const inputNum = document.querySelector("#income-amount");
+		incomes.push({
+			name: inputText.value,
+			amount: inputNum.value,
+			id: Math.random(),
+		});
+		renderList(incomes, "#list-incomes");
+		inputText.value = "";
+		inputNum.value = "";
+	});
+
+// const sumLi = (items, listId) => {
+// 	const sumOutput = document.querySelector(listId);
+// 	list.innerHTML = "";
+// 	items.reduce((acc, number) => {
+// 		const itemOutput = document.createTextNode(element.amount);
+// 		return acc + number;
+// 	}, 0);
+// 		list.appendChild(itemNum);
+// }
+
+document
+	.querySelector("#expense-form")
+	.addEventListener("submit", function (event) {
+		event.preventDefault();
+		const inputText = document.querySelector("#expense-text");
+		const inputNum = document.querySelector("#expense-amount");
+
+		expenses.push({
+			name: inputText.value,
+			amount: inputNum.value,
+			id: Math.random(),
+		});
+		renderList(expenses, "#list-expenses");
+		inputText.value = "";
+		inputNum.value = "";
+	});
